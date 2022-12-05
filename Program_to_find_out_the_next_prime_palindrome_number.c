f9bf33bd8e6347b19d28f71f128123f6af67b412
sanjit22a91a05k1 <119840566+sanjit22a91a05k1@users.noreply.github.com>
2022-12-06 00:49:38 +0530

    # include<stdio.h> int prime(int); int pal(int ); int main() {     int n,f;     scanf("%d",&n);     for(int i=n+1; ; i++)     {         if(pal(i) && prime(i))         {             f=i;             break;         }     }     printf("%d",f); } int prime(int n) {     int c=0;     for(int i=1; i<=n; i++)     {         if(n%i==0)         {             c++;         }     }     if(c==2)     {         return 1;     }     else     {         return 0;     } } int pal(int n) {     int r,rev=0;     int t=n;     while(n!=0)     {         r=n%10;         rev=rev*10+r;         n=n/10;     }     if(rev==t)     {         return 1;     }     else     {         return 0;     } }

---
+++ Program_to_find_out_the_next_prime_palindrome_number.c
@@ -0,0 +1,55 @@
+# include<stdio.h>
+int prime(int);
+int pal(int );
+int main()
+{
+    int n,f;
+    scanf("%d",&n);
+    for(int i=n+1; ; i++)
+    {
+        if(pal(i) && prime(i))
+        {
+            f=i;
+            break;
+        }
+    }
+    printf("%d",f);
+}
+int prime(int n)
+{
+    int c=0;
+    for(int i=1; i<=n; i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+        }
+    }
+    if(c==2)
+    {
+        return 1;
+    }
+    else 
+    {
+        return 0;
+    }
+}
+int pal(int n)
+{
+    int r,rev=0;
+    int t=n;
+    while(n!=0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    if(rev==t)
+    {
+        return 1;
+    }
+    else 
+    {
+        return 0;
+    }
+}
