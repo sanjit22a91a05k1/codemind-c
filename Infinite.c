315d9c8fc4cbfe355b5133cd26a4019cbac52b82
sanjit22a91a05k1 <119840566+sanjit22a91a05k1@users.noreply.github.com>
2022-12-06 10:09:56 +0530

    # include<stdio.h> int main() {     int n,s;     scanf("%d",&n);     while(n!=-1)     {         s=n*n;          printf("%d ",s);          scanf("%d",&n);     }
    }

---
+++ Infinite.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+int main()
+{
+    int n,s;
+    scanf("%d",&n);
+    while(n!=-1)
+    {
+        s=n*n;
+         printf("%d
+",s);
+         scanf("%d",&n);
+    }
+    
+   
+}
