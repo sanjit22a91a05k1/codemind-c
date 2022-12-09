4ca699cfa2d519cbad93fdf8a3103de1f9e97a03
sanjit22a91a05k1 <119840566+sanjit22a91a05k1@users.noreply.github.com>
2022-12-09 16:19:30 +0530

    # include<stdio.h> int main() { int n,c=0; scanf("%d",&n); int a[n]; for(int i=0; i<n; i++) { scanf("%d",&a[i]); } int min=a[0]; for(int i=0; i<n; i++ ) { if(min>a[i]) { min=a[i]; } } for(int i=0; i<n; i++) { if(a[i]%min==0) { c++; } } if(c==n) { printf("%d",min); } else { printf("1"); } }

---
+++ HCF_of_an_array.c
@@ -0,0 +1,34 @@
+# include<stdio.h>
+int main()
+{
+int n,c=0;
+scanf("%d",&n);
+int a[n];
+for(int i=0; i<n; i++)
+{
+scanf("%d",&a[i]);
+}
+int min=a[0];
+for(int i=0; i<n; i++ )
+{
+if(min>a[i])
+{
+min=a[i];
+}
+}
+for(int i=0; i<n; i++)
+{
+if(a[i]%min==0)
+{
+c++;
+}
+}
+if(c==n)
+{
+printf("%d",min);
+}
+else
+{
+printf("1");
+}
+}
