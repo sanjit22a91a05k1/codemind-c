207f6afebfb7e2559fb5bb6969ecfaa261d29c95
sanjit22a91a05k1 <119840566+sanjit22a91a05k1@users.noreply.github.com>
2022-12-11 03:34:43 +0530

    # include<stdio.h> int main() {     int n,c=0,count=0;     scanf("%d",&n);     int a[n];     for(int i=0; i<n; i++)     {         scanf("%d",&a[i]);     }     for(int i=0; i<n; i++)     {         if(i%2==0)         {             if(a[i]%2==0)             {                 c++;             }         }         else         {             if(a[i]%2==0)             {                 count++;             }         }     }     if(count==0)     {         printf("True");     }     else     {         printf("False");     } }

--- Strictly_EVEN.c
+++ Strictly_EVEN.c
@@ -1,37 +1,36 @@
 # include<stdio.h>
 int main()
 {
-     int n; 
-     scanf("%d",&n);
-     int a[n];
-     for(int i=0; i<n; i++)
-     {
-         scanf("%d",&a[i]);
-     }
-    int c=0,count=0;
-     for(int i=0;i<n; i++)
-     {
-         if(i%2==0)
-         {
+    int n,c=0,count=0;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0; i<n; i++)
+    {
+        if(i%2==0)
+        {
             if(a[i]%2==0)
             {
                 c++;
             }
-         }
-         else 
-         {
-             if(a[i]%2==0)
-             {
-                count++; 
-             }
-         }
-         }
-         if(count==0)
-         {
-             printf("True");
-         }
-         else
-         { 
-       printf("False");
-         }
+        }
+        else 
+        {
+            if(a[i]%2==0)
+            {
+                count++;
+            }
+        }
+    }
+    if(count==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
 }
