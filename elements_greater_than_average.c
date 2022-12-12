4e3146e684f17d94eed178de502977704a5c3c3c
sanjit22a91a05k1 <119840566+sanjit22a91a05k1@users.noreply.github.com>
2022-12-12 11:51:46 +0530

    # include<stdio.h> int main() {     int n,s=0,c=0;     scanf("%d",&n);     int a[n];     for(int i=0; i<n; i++)     {         scanf("%d",&a[i]);     }     for(int i=0; i<n; i++)     {         s=s+a[i];     }     int ave=s/n;     for(int i=0; i<n; i++)     {         if(a[i]>=ave)         {             c++;         }     }     printf("%d",c); }

---
+++ elements_greater_than_average.c
@@ -0,0 +1,24 @@
+# include<stdio.h>
+int main()
+{
+    int n,s=0,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(int i=0; i<n; i++)
+    {
+        s=s+a[i];
+    }
+    int ave=s/n;
+    for(int i=0; i<n; i++)
+    {
+        if(a[i]>=ave)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
