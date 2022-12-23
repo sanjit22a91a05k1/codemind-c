909a214f0bac76a868750be92a17e298276a4ac4
Sanjit Das <119840566+sanjit22a91a05k1@users.noreply.github.com>
2022-12-23 20:39:08 +0530

    # include<stdio.h> int main() {     float c,h;     scanf("%f",&h);     c=2.54*h;     printf("%.2f",c); }

--- inches_to_centimeters.c
+++ inches_to_centimeters.c
@@ -1,8 +1,8 @@
-#include<stdio.h>
+# include<stdio.h>
 int main()
 {
- float h,c;
- scanf("%f",&h);
- c=2.54*h;;
- printf("%.2f",c);
+    float c,h;
+    scanf("%f",&h);
+    c=2.54*h;
+    printf("%.2f",c);
 }
