ad39d1e01128e914c28dbf85342c0572c3104085
Sanjit Das <119840566+sanjit22a91a05k1@users.noreply.github.com>
2022-12-26 10:05:51 +0530

    # include<stdio.h> int main() {     int n,c=0;     scanf("%d",&n);     int a[n];     for(int i=0; i<n; i++)     {         scanf("%d",&a[i]);     }     int p,q,f=1;     scanf("%d%d",&p,&q);     int max=-10000;     for(int i=0; i<n; i++)     {         if(a[i]>p&&a[i]<q)         {             c++;         }         else         {            if(a[i]>max)            {                max=a[i];                f=0;            }         }
        }
        if(f==0)
        {
            printf("%d",max);
        }
         if(f==1)
         {
             printf("-1");
         }
    }

---
+++ maximum_of_numbers_not_between_A_and_B.c
@@ -0,0 +1,38 @@
+# include<stdio.h>
+int main()
+{
+    int n,c=0;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int p,q,f=1;
+    scanf("%d%d",&p,&q);
+    int max=-10000;
+    for(int i=0; i<n; i++)
+    {
+        if(a[i]>p&&a[i]<q) 
+        {
+            c++;
+        }
+        else 
+        {
+           if(a[i]>max)
+           {
+               max=a[i];
+               f=0;
+           }
+        }
+    
+    }
+    if(f==0)
+    {
+        printf("%d",max);
+    }
+     if(f==1)
+     {
+         printf("-1");
+     }
+}
