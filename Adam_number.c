66146a13cf5a7eab193ad83f0638f3e8e9484c88
Sanjit Das <119840566+sanjit22a91a05k1@users.noreply.github.com>
2022-12-28 18:33:49 +0530

    # include<stdio.h> int main() { int n,r,rem,re=0,rev=0; scanf("%d",&n); int sq=n*n; while(n!=0) { r=n%10; rev=rev*10+r; n=n/10; } int s=rev*rev; while(s!=0) { rem=s%10; re=re*10+rem; s=s/10; } if(re==sq) { printf("True"); } else { printf("False"); } }

---
+++ Adam_number.c
@@ -0,0 +1,28 @@
+# include<stdio.h>
+int main()
+{
+int n,r,rem,re=0,rev=0;
+scanf("%d",&n);
+int sq=n*n;
+while(n!=0)
+{
+r=n%10;
+rev=rev*10+r;
+n=n/10;
+}
+int s=rev*rev;
+while(s!=0)
+{
+rem=s%10;
+re=re*10+rem;
+s=s/10;
+}
+if(re==sq)
+{
+printf("True");
+}
+else 
+{
+printf("False");
+}
+}
