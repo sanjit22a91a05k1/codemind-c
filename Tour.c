fa89fcdcf044a0a35bdf42bb0cbd2eb0a0042d05
Sanjit Das <119840566+sanjit22a91a05k1@users.noreply.github.com>
2022-12-28 23:49:51 +0530

    #include<stdio.h> int main() {     int t;     scanf("%d",&t);     for(int i=1; i<=t; i++)     {         int m,n;         scanf("%d%d",&n,&m);         int a=5*n+m*7;         printf("%d ",a);     } }

---
+++ Tour.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    for(int i=1; i<=t; i++)
+    {
+        int m,n;
+        scanf("%d%d",&n,&m);
+        int a=5*n+m*7;
+        printf("%d
+",a);
+    }
+}
