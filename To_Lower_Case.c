94bc6825ca813206699c793bd4b2c0ed2ebaf53b
Sanjit Das <119840566+sanjit22a91a05k1@users.noreply.github.com>
2023-01-09 00:03:01 +0530

    # include<stdio.h> # include<string.h> int main() {     char s1[100];     scanf("%s",s1);     for(int i=0; s1[i]!=NULL; i++)     {         if(s1[i]>=65 && s1[i]<=90)         {             s1[i]=s1[i]+32;         }     }     printf("%s",s1); }

---
+++ To_Lower_Case.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+# include<string.h>
+int main()
+{
+    char s1[100];
+    scanf("%s",s1);
+    for(int i=0; s1[i]!=NULL; i++)
+    {
+        if(s1[i]>=65 && s1[i]<=90)
+        {
+            s1[i]=s1[i]+32;
+        }
+    }
+    printf("%s",s1);
+}
