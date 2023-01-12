311c284599f98cf6619652f316fd2007f7265795
Sanjit Das <119840566+sanjit22a91a05k1@users.noreply.github.com>
2023-01-12 19:32:56 +0530

    # include<stdio.h> int main() {     int r,c,max1=0,max2=0;     scanf("%d%d",&r,&c);     int a[r][c];     for(int i=0; i<r; i++)     {         for(int j=0; j<c; j++)         {         scanf("%d",&a[i][j]);             }     }     for(int i=0; i<r; i++)     {         int sum=0;         for(int j=0; j<c; j++)         {             sum=sum+a[i][j];             }             if(sum>max1)             {                 max1=sum;             }
        } 
        for(int i=0; i<c; i++)
        { 
            int sum=0;
            for(int j=0; j<r; j++)
            {
                sum=sum+a[j][i]; 
                
            }
            if(sum>max2) 
            { 
                max2=sum;
                }
                }
                if(max1<max2) 
                { 
                    max1=max2;
                    } 
                    printf("%d",max1);
        
    }

--- Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.c
+++ Maximum_sum_of_row_wise_sum_or_column_wise_sum_of_matrix.c
@@ -1,43 +1,46 @@
 # include<stdio.h>
 int main()
-{
-    int r,c,max1=0,max2=0;
-    scanf("%d%d",&r,&c);
-    int a[r][c];
-    for(int i=0; i<r; i++)
-    {
+{ 
+    int r,c,max1=0,max2=0; 
+    scanf("%d%d",&r,&c); 
+    int a[r][c]; 
+    for(int i=0; i<r; i++) 
+    { 
         for(int j=0; j<c; j++)
-        {
-            scanf("%d",&a[i][j]);
-        }
-    }
-    for(int i=0; i<r; i++)
-    {
-       int sum=0;
+        { 
+        scanf("%d",&a[i][j]);
+            } 
+    } 
+    for(int i=0; i<r; i++) 
+    { 
+        int sum=0; 
         for(int j=0; j<c; j++)
-        {
-         sum=sum+a[i][j];   
-        }
-        if(sum>max1)
-        {
-            max1=sum;
-        }
-    }
+        { 
+            sum=sum+a[i][j];
+            } 
+            if(sum>max1)
+            { 
+                max1=sum;
+            } 
+        
+    } 
     for(int i=0; i<c; i++)
-    {
-       int sum=0;
+    { 
+        int sum=0;
         for(int j=0; j<r; j++)
         {
-         sum=sum+a[j][i];   
+            sum=sum+a[j][i]; 
+            
         }
-        if(sum>max2)
-        {
+        if(sum>max2) 
+        { 
             max2=sum;
-        }
-    }
-    if(max1<max2)
-    {
-        max1=max2;
-    }
-    printf("%d",max1);
+            }
+            }
+            if(max1<max2) 
+            { 
+                max1=max2;
+                } 
+                printf("%d",max1);
+    
 }
