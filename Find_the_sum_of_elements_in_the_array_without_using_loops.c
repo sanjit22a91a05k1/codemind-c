019826d2443dc3d3223429a4dfbd32dd73786565
Sanjit Das <119840566+sanjit22a91a05k1@users.noreply.github.com>
2023-01-14 00:46:32 +0530

    # include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n];     int s=0;     for(int i=0; i<n; i++)     {         scanf("%d",&a[i]);         s=s+a[i];     }     printf("%d",s); }

---
+++ Find_the_sum_of_elements_in_the_array_without_using_loops.c
@@ -0,0 +1,14 @@
+# include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    int s=0;
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+        s=s+a[i];
+    }
+    printf("%d",s);
+}
