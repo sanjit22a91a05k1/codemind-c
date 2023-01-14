a09a53471fed1f43e33afe75b70563a0ea581404
Sanjit Das <119840566+sanjit22a91a05k1@users.noreply.github.com>
2023-01-14 15:39:40 +0530

    # include<stdio.h> int prime(int); int main() {     int n,f,b;     scanf("%d",&n);     for(int i=n; ; i++)     {         if(prime(i))         {             f=i;             break;         }     }     for(int i=n; ; i--)     {         if(prime(i))         {             b=i;             break;         }     }     int d1=f-n;     int d2=n-b;     if(d1>d2)     {         printf("%d",d2);     }     else     {         printf("%d",d1);     } } int prime(int n) {     int c=0;     for(int i=1; i<=n; i++)     {         if(n%i==0)         {             c++;         }     }     if(c==2)     {         return 1;     }     else     {         return 0;     } }

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.c
@@ -0,0 +1,52 @@
+# include<stdio.h>
+int prime(int);
+int main()
+{
+    int n,f,b;
+    scanf("%d",&n);
+    for(int i=n; ; i++)
+    {
+        if(prime(i))
+        {
+            f=i;
+            break;
+        }
+    }
+    for(int i=n; ; i--)
+    {
+        if(prime(i))
+        {
+            b=i;
+            break;
+        }
+    }
+    int d1=f-n;
+    int d2=n-b;
+    if(d1>d2)
+    {
+        printf("%d",d2);
+    }
+    else 
+    {
+        printf("%d",d1);
+    }
+}
+int prime(int n)
+{
+    int c=0;
+    for(int i=1; i<=n; i++)
+    {
+        if(n%i==0)
+        {
+            c++;
+        }
+    }
+    if(c==2)
+    {
+        return 1;
+    }
+    else 
+    {
+        return 0;
+    }
+}
