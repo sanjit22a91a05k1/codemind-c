8495587fb58b12371e91b3b3ce498c2e76dddea2
Sanjit Das <119840566+sanjit22a91a05k1@users.noreply.github.com>
2023-01-14 20:16:25 +0530

    # include<stdio.h> # include<math.h> int main() {     float a,b,c,s,A;     scanf("%f%f%f",&a,&b,&c);     s=(a+b+c)/2;     A=sqrt(s*(s-a)*(s-b)*(s-c));     printf("%.2f",A); }

---
+++ Area_of_Triangle_.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+# include<math.h>
+int main()
+{
+    float a,b,c,s,A;
+    scanf("%f%f%f",&a,&b,&c);
+    s=(a+b+c)/2;
+    A=sqrt(s*(s-a)*(s-b)*(s-c));
+    printf("%.2f",A);
+}
