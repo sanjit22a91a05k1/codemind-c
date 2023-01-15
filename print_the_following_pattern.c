c41fb0a6673910d19cdd285baa702bdec1637c3b
Sanjit Das <119840566+sanjit22a91a05k1@users.noreply.github.com>
2023-01-15 17:21:45 +0530

    # include<stdio.h> int main() {     int n;     scanf("%d",&n);     for(int i=1; i<=n; i++)     {         for(int k=1; k<=n-i; k++)         {             printf(" ");         }         for(int j=1; j<=n; j++)         {             if(i==1 || i==n || j==1 || j==n)             {                 printf("*");             }             else             {                 printf(" ");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,9 +5,20 @@ int main()
     scanf("%d",&n);
     for(int i=1; i<=n; i++)
     {
+        for(int k=1; k<=n-i; k++)
+        {
+            printf(" ");
+        }
         for(int j=1; j<=n; j++)
         {
-            printf("%c ",i+64);
+            if(i==1 || i==n || j==1 || j==n)
+            {
+                printf("*");
+            }
+            else 
+            {
+                printf(" ");
+            }
         }
         printf("
 ");
