357fb250592843209ab3a5bf24247c7cb32c2ca7
Sanjit Das <119840566+sanjit22a91a05k1@users.noreply.github.com>
2023-05-18 01:12:22 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n];     int f=1;     for(int i=0; i<n; i++)     {         scanf("%d",&a[i]);     }     int k;     scanf("%d",&k);     for(int i=0; i<n; i++)     {         int c=0;         for(int j=0; j<n; j++)         {             if(a[i]==a[j])             {                 c++;             }         }         if(k==c)         {             printf("%d ",a[i]);             f=0;             for(int k=0; k<n; k++)             {                 if(a[i]==a[k])                 {                     a[k]=0;                 }             }         }     }     if(f==1)     {         printf("-1");     } }

---
+++ N_times_repeated_elements.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    int f=1;
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int k;
+    scanf("%d",&k);
+    for(int i=0; i<n; i++)
+    {
+        int c=0;
+        for(int j=0; j<n; j++)
+        {
+            if(a[i]==a[j])
+            {
+                c++;
+            }
+        }
+        if(k==c)
+        {
+            printf("%d ",a[i]);
+            f=0;
+            for(int k=0; k<n; k++)
+            {
+                if(a[i]==a[k])
+                {
+                    a[k]=0;
+                }
+            }
+        }
+    }
+    if(f==1)
+    {
+        printf("-1");
+    }
+}
