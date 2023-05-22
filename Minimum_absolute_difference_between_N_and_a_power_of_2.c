c731c9cad862d7f2940d5202050fd695ca1a01e0
Sanjit Das <119840566+sanjit22a91a05k1@users.noreply.github.com>
2023-05-23 00:13:45 +0530

    #include<stdio.h> #include<math.h> int powe(int n,int x) { int p=1; for(int i=1; i<=x; i++) {     p=p*n; } return p; } int main() {     int n;     scanf("%d",&n);     int x=0;     int i=0;     while(x<=n)     {       x=powe(2,i);       i++;     }     int b=x/2;     int d1=x-n;     int d2=n-b;     if(d1>d2)     {         printf("%d",d2);     }     else     {         printf("%d",d1);     } }

--- Minimum_absolute_difference_between_N_and_a_power_of_2.c
+++ Minimum_absolute_difference_between_N_and_a_power_of_2.c
@@ -1,25 +1,34 @@
-# include<stdio.h>
-# include<math.h>
+#include<stdio.h>
+#include<math.h>
+int powe(int n,int x)
+{
+int p=1;
+for(int i=1; i<=x; i++)
+{
+    p=p*n;
+}
+return p;
+}
 int main()
 {
-     int n,x=0;
-     scanf("%d",&n);
-    int  i=1;
-     while(x<=n)
-     {
-     x=pow(2,i);
-         i++;
-     }
-     int f=x;
-    int b=f/2;
-     int d1=f-n;
-    int  d2=n-b;
-     if(d1>=d2)
-     {
-         printf("%d",d2);
-     }
-     else if (d2>d1)
-     {
-         printf("%d",d1);
-     }
+    int n;
+    scanf("%d",&n);
+    int x=0;
+    int i=0;
+    while(x<=n)
+    {
+      x=powe(2,i);
+      i++;
+    }
+    int b=x/2;
+    int d1=x-n;
+    int d2=n-b;
+    if(d1>d2)
+    {
+        printf("%d",d2);
+    }
+    else
+    {
+        printf("%d",d1);
+    }
 }
